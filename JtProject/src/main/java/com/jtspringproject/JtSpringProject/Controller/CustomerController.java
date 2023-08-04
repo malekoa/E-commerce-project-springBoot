fff7805e86c1071e2a61ba1ddecbb7893264bd93
TheRealSal <75696327+TheRealSal@users.noreply.github.com>
2023-08-04 18:26:51 -0400

    Added javadoc comments for controllers - Added documentation for Customer entity as well

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/Controller/CustomerController.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/Controller/CustomerController.java
@@ -8,62 +8,109 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+/**
+ * The CustomerController class handles HTTP requests related to customer actions such as adding to cart,
+ * viewing cart, applying and removing coupons, checkout, and customer registration.
+ */
 @Controller
-public class CustomerController{
-    private CustomerService service;
-    // Use cases:
-    // Add to cart
-    // View Cart - Done
-    // Checkout
-    // Delete Account
+public class CustomerController {
+    private CustomerService service; // Service class to handle customer-related operations
 
-    //
+    /**
+     * Retrieves the cart details for the specified customer from the database.
+     *
+     * @param customerID The ID of the customer whose cart is to be retrieved.
+     * @return The Cart object representing the customer's cart.
+     */
     @GetMapping("/cart")
-    public Cart viewCart(@RequestParam("customerId") int customerID)
-    {
+    public Cart viewCart(@RequestParam("customerId") int customerID) {
         return service.getCartById(customerID);
     }
 
-
+    /**
+     * Adds an item with the specified quantity to the customer's cart.
+     *
+     * @param customerID The ID of the customer.
+     * @param itemID     The ID of the item to be added to the cart.
+     * @param quantity   The quantity of the item to be added.
+     * @return A string indicating the status of the operation (e.g., success or failure).
+     */
     @PostMapping("/addToCart")
     public String addToCart(@RequestParam("customerId") int customerID,
                             @RequestParam("ItemID") int itemID,
-                            @RequestParam("quantity") int quantity){
+                            @RequestParam("quantity") int quantity) {
         return service.addToCart(customerID, itemID, quantity);
     }
 
+    /**
+     * Applies a coupon to the customer's cart.
+     *
+     * @param customerID The ID of the customer.
+     * @param cartId     The ID of the cart to which the coupon is to be applied.
+     * @return A string indicating the status of the operation (e.g., success or failure).
+     */
     @PostMapping("/applyCoupon")
     public String applyCoupon(@RequestParam("customerId") int customerID,
-                            @RequestParam("cartId") int cartId){
+                              @RequestParam("cartId") int cartId) {
         return service.applyCoupon(customerID, cartId);
     }
 
+    /**
+     * Removes a coupon from the customer's cart.
+     *
+     * @param customerID The ID of the customer.
+     * @param cartId     The ID of the cart from which the coupon is to be removed.
+     * @return A string indicating the status of the operation (e.g., success or failure).
+     */
     @PostMapping("/removeCoupon")
     public String removeCoupon(@RequestParam("customerId") int customerID,
-                            @RequestParam("cartId") int cartId){
+                               @RequestParam("cartId") int cartId) {
         return service.removeCoupon(customerID, cartId);
     }
 
+    /**
+     * Initiates the checkout process for the customer's cart.
+     *
+     * @param customerID The ID of the customer.
+     * @param cartId     The ID of the cart to be checked out.
+     * @return A string indicating the status of the checkout process (e.g., success or failure).
+     */
     @PostMapping("/checkout")
     public String checkout(@RequestParam("customerId") int customerID,
-                            @RequestParam("cartId") int cartId){
+                           @RequestParam("cartId") int cartId) {
         return service.checkout(customerID, cartId);
     }
 
+    /**
+     * Confirms and places the order for the items in the customer's cart.
+     *
+     * @param customerID The ID of the customer.
+     * @param cartId     The ID of the cart containing the items to be ordered.
+     * @return A string indicating the status of the order confirmation (e.g., success or failure).
+     */
     @PostMapping("/confirmOrder")
     public String confirmOrder(@RequestParam("customerId") int customerID,
-                            @RequestParam("cartId") int cartId){
+                               @RequestParam("cartId") int cartId) {
         return service.confirmOrder(customerID, cartId);
     }
 
-
+    /**
+     * Registers a new customer with the provided username, password, email, and address.
+     * The password is hashed before saving it to the database.
+     *
+     * @param username The username of the new customer.
+     * @param password The password of the new customer.
+     * @param email    The email address of the new customer.
+     * @param address  The address of the new customer.
+     * @return The view name "userlogin" if the customer is registered successfully; otherwise, redirects to "error".
+     */
     @PostMapping("/register")
-    public String newCustomerRegister(@RequestParam("username") String username,@RequestParam("password") String password,
-                                      @RequestParam("email") String email, @RequestParam("address") String address)
-    {// TODO - hash password before saving
-        try
-        {
+    public String newCustomerRegister(@RequestParam("username") String username,
+                                      @RequestParam("password") String password,
+                                      @RequestParam("email") String email,
+                                      @RequestParam("address") String address) {
+        // TODO - hash password before saving
+        try {
             Customer customer = new Customer();
             customer.setUsername(username);
             customer.setPassword(password);
@@ -72,9 +119,7 @@ public class CustomerController{
             service.createCustomer(customer);
             System.out.println("Created customer");
             return "userlogin";
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             System.out.println("Exception:"+e);
             return "redirect:/error";
         }
--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/Entity/Customer.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/Entity/Customer.java
@@ -1,7 +1,12 @@
 package com.jtspringproject.JtSpringProject.Entity;
 
 import javax.persistence.*;
-
+/**
+ * The Customer class represents a customer in the system.
+ * It extends the User class and adds additional attributes
+ * such as name, email, cart ID, number of coupons, and
+ * coupon accumulator.
+ */
 @Entity
 @PrimaryKeyJoinColumn(name = "id")
 @Table(name = "Customer")
@@ -11,10 +16,23 @@ public class Customer extends User{
     private String cartID;
     private int numCoupons;
     private double couponAccumulator;
-    private Cart cart;  // Added Cart attribute
     private String address;
 
-    // Constructors
+    /**
+     * Constructs a new Customer object with the specified
+     * ID, username, password, name, email, cart ID, number
+     * of coupons, and address.
+     *
+     * @param id the ID of the customer
+     * @param username the username of the customer
+     * @param password the password of the customer
+     * @param name the name of the customer
+     * @param email the email of the customer
+     * @param cartID the ID of the customer's cart
+     * @param numCoupons the number of coupons the customer has
+     * @param couponAccumulator the total value of the customer's coupons
+     * @param address the address of the customer
+     */
     public Customer(int id, String username, String password, String name, String email, String cartID, int numCoupons, int couponAccumulator, String address) {
         super(id, username, password, "Customer");
         this.name = name;
@@ -25,68 +43,135 @@ public class Customer extends User{
         this.address = address;
     }
 
+    /**
+     * Constructs a new Customer object with default values.
+     */
     public Customer() {
         super();
     }
 
-    // Accessor Methods
+    /**
+     * Returns the total value of the customer's coupons.
+     *
+     * @return the coupon accumulator
+     */
     public double getCouponAccumulator() {
         return couponAccumulator;
     }
 
+    /**
+     * Returns the number of coupons the customer has.
+     *
+     * @return the number of coupons
+     */
     public int getNumCoupons() {
         return numCoupons;
     }
 
+    /**
+     * Returns the ID of the customer's cart.
+     *
+     * @return the cart ID
+     */
     public String getCartID() {
         return cartID;
     }
 
+    /**
+     * Returns the email of the customer.
+     *
+     * @return the email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Returns the name of the customer.
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
-
+    /**
+     * Returns the address of the customer.
+     *
+     * @return the address
+     */
     public String getAddress() {
         return address;
     }
 
-    // Mutator Methods
+    /**
+     * Sets the address of the customer.
+     *
+     * @param address the new address
+     */
     public void setAddress(String address) {
         this.address = address;
     }
 
-
-    // Mutator Methods
-
+    /**
+     * Sets the total value of the customer's coupons.
+     *
+     * @param couponAccumulator the new coupon accumulator
+     */
     public void setCouponAccumulator(double couponAccumulator) {
         this.couponAccumulator = couponAccumulator;
     }
 
+    /**
+     * Sets the number of coupons the customer has.
+     *
+     * @param numCoupons the new number of coupons
+     */
     public void setNumCoupons(int numCoupons) {
         this.numCoupons = numCoupons;
     }
 
+    /**
+     * Sets the ID of the customer's cart.
+     *
+     * @param cartID the new cart ID
+     */
     public void setCartID(String cartID) {
         this.cartID = cartID;
     }
 
+    /**
+     * Sets the email of the customer.
+     *
+     * @param email the new email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Sets the name of the customer.
+     *
+     * @param name the new name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Sets the customer's cart.
+     *
+     * @param cart the new cart
+     */
     public void setCart(Cart cart) {  // Added setCart method
-        this.cart = cart;
+        // Copies all items from cart and adds them to customer's cart
     }
 
+    /**
+     * Returns a string representation of the customer.
+     *
+     * @return a string representation of the customer
+     */
     @Override
     public String toString(){
         return "Customer{" +
