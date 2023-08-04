2b4cb3cdfd16601df5d54d539b37860d5d3ef407
TheRealSal <75696327+TheRealSal@users.noreply.github.com>
2023-08-04 18:37:55 -0400

    Added javadoc comments for service files

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/Service/CustomerService.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/Service/CustomerService.java
@@ -4,43 +4,104 @@ import com.jtspringproject.JtSpringProject.Entity.Cart;
 import com.jtspringproject.JtSpringProject.Entity.Customer;
 import com.jtspringproject.JtSpringProject.Repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+/**
+ * Service class for managing Customer entities.
+ */
+@Service
 public class CustomerService {
+
     @Autowired
     private CustomerRepository repository;
 
+    /**
+     * Creates a new customer.
+     *
+     * @param customer The Customer entity to create.
+     * @return The created Customer entity.
+     */
     public Customer createCustomer(Customer customer){
         return repository.save(customer);
     }
 
+    /**
+     * Retrieves a customer by their email.
+     *
+     * @param email The email of the customer to retrieve.
+     * @return The Customer entity with the provided email, or null if not found.
+     */
     public Customer findByEmail(String email){
         return repository.findByEmail(email);
     }
 
+    /**
+     * Retrieves the cart of a customer by their ID.
+     *
+     * @param id The ID of the customer whose cart to retrieve.
+     * @return The Cart entity associated with the provided customer ID.
+     */
     public Cart getCartById(Integer id){
         return repository.getCartById(id);
     }
 
+    /**
+     * Adds an item to the customer's cart.
+     *
+     * @param customerID The ID of the customer.
+     * @param itemID The ID of the item to add.
+     * @param quantity The quantity of the item to add.
+     * @return The URL to redirect after adding the item to the cart.
+     */
     public String addToCart(int customerID, int itemID, int quantity) {
         return "/cart";
     }
 
+    /**
+     * Applies a coupon to the customer's cart.
+     *
+     * @param customerID The ID of the customer.
+     * @param cartId The ID of the cart.
+     * @return The URL to redirect after applying the coupon.
+     */
     public String applyCoupon(int customerID, int cartId) {
         return "/cart";
     }
 
+    /**
+     * Removes a coupon from the customer's cart.
+     *
+     * @param customerID The ID of the customer.
+     * @param cartId The ID of the cart.
+     * @return The URL to redirect after removing the coupon.
+     */
     public String removeCoupon(int customerID, int cartId) {
         return "/cart";
     }
 
+    /**
+     * Processes the checkout for the customer's cart.
+     *
+     * @param customerID The ID of the customer.
+     * @param cartId The ID of the cart.
+     * @return The URL to redirect after the checkout process.
+     */
     public String checkout(int customerID, int cartId) {
         return "/cart";
     }
 
+    /**
+     * Confirms the customer's order.
+     *
+     * @param customerID The ID of the customer.
+     * @param cartId The ID of the cart.
+     * @return The URL to redirect after confirming the order.
+     */
     public String confirmOrder(int customerID, int cartId) {
         return "/cart";
     }
-//
+
+    //
 //    public String addToCart(int customerID, int itemID, int quantity) {
 //        return repository.addToCart(customerID, itemID, quantity);
 //    }
--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/Service/UserService.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/Service/UserService.java
@@ -7,26 +7,53 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+/**
+ * Service class for managing User entities.
+ */
 @Service
 public class UserService {
+
     @Qualifier("userRepository")
     @Autowired
     private UserRepository repository;
 
+    /**
+     * Creates a new user.
+     *
+     * @param user The User entity to create.
+     * @return The created User entity.
+     */
     public User createUser(User user){
         return repository.save(user);
     }
 
+    /**
+     * Retrieves a list of all users.
+     *
+     * @return A List containing all User entities.
+     */
     public List<User> getAllUsers(){
         return repository.findAll();
     }
 
+    /**
+     * Retrieves a user by their ID.
+     *
+     * @param id The ID of the user to retrieve.
+     * @return The User entity with the provided ID, or null if not found.
+     */
     public User getUserById(int id){
         return repository.findById(id).orElse(null);
     }
 
+    /**
+     * Retrieves a user by their username.
+     *
+     * @param username The username of the user to retrieve.
+     * @return The User entity with the provided username, or null if not found.
+     */
     public User getUserByName(String username){
         return repository.findByUsername(username);
     }
 }
+
